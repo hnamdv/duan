a06c84c062ab5347e06d78f63729c47457cf77ea
hnamdv <hoangnam0201deptrai@gmail.com>
2025-05-16 22:04:40 +0700

    nam

---
+++ src/UI/Controller/XAuth.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI.Controller;
+
+import ENTITY.NguoiDung;
+
+/**
+ *
+ * @author Admin
+ */
+
+public class XAuth {
+    public static NguoiDung user = new NguoiDung(
+        1,                 
+        "Doãn Chí Tèo",   
+        "123"          
+     
+    );
+}
